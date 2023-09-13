1a5c0bc9669913c9c283b76bff995665ecd29ae3
alessandrodaniello105 <kanymsevement@gmail.com>
2023-09-13 15:27:17 +0200

    [add]: cycle for images

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
-const arrayImgs = [];
+/* 2.
+*/
+
+const arrayImgs = [
+  'img/01.webp',
+  'img/02.webp',
+  'img/03.webp',
+  'img/04.webp',
+  'img/05.webp'
+];
+console.log(arrayImgs.length);
 
 const itemsCollector = document.querySelector('.items-wrapper');
-const btnPrev = document.querySelector('.up')
-const btnNext = document.querySelector('.down')
+const btnPrev = document.querySelector('.up');
+const btnNext = document.querySelector('.down');
+
+for (let i = 0; i < arrayImgs.length; i++ ) {
+  let image = arrayImgs[i];
+  itemsCollector.innerHTML = `<img src="${image}" class="hide item">`;
+  console.log(itemsCollector.innerHTML);
+};
 
 
-btnPrev.addEventListener('click', function(){
-  console.log('vado alla foto precedente')
+
+btnNext.addEventListener('click', function(){
+  
 });
-
-
-itemsCollector.innerHTML = "pane";
